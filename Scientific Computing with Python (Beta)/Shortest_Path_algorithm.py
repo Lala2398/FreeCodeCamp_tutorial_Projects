ca811fb8f3a51ca55d3077c5479d82c3edfcb236
Lala Ibadullayeva <86626835+Lala2398@users.noreply.github.com>
2024-12-16 01:20:45 +0400

    Create Shortest_Path_algorithm.py

---
+++ Scientific Computing with Python (Beta)/Shortest_Path_algorithm.py
@@ -0,0 +1,46 @@
+my_graph = {
+    'A': [('B', 5), ('C', 3), ('E', 11)],
+    'B': [('A', 5), ('C', 1), ('F', 2)],
+    'C': [('A', 3), ('B', 1), ('D', 1), ('E', 5)],
+    'D': [('C',1 ), ('E', 9), ('F', 3)],
+    'E': [('A', 11), ('C', 5), ('D', 9)],
+    'F': [('B', 2), ('D', 3)]
+}
+
+def shortest_path(graph, start, target = ''):
+    unvisited = list(graph)
+    distances = {node: 0 if node == start else float('inf') for node in graph}
+    paths = {node: [] for node in graph}
+    paths[start].append(start)
+    
+    while unvisited:
+        current = min(unvisited, key=distances.get)
+        for node, distance in graph[current]:
+            if distance + distances[current] < distances[node]:
+                distances[node] = distance + distances[current]
+                if paths[node] and paths[node][-1] == node:
+                    paths[node] = paths[current][:]
+                else:
+                    paths[node].extend(paths[current])
+                paths[node].append(node)
+        unvisited.remove(current)
+    
+    targets_to_print = [target] if target else graph
+    for node in targets_to_print:
+        if node == start:
+            continue
+        print(f'\n{start}-{node} distance: {distances[node]}\nPath: {" -> ".join(paths[node])}')
+    
+    return distances, paths
+    
+shortest_path(my_graph, 'A','F')
+
+
+#Task : 
+#Algorithms are step-by-step procedures that developers use to perform calculations and solve computational problems.
+#In this project, you'll learn how to use functions, loops, conditional statements, and dictionary comprehensions to implement a Shortest Path algorithm.
+
+#What is Shortest Path? : 
+# The shortest path refers to the minimum distance or cost required to travel from one node (or point) to another in a graph. 
+#This is widely used in computer science, mathematics, and real-world applications like navigation systems, network routing, and logistics. 
+#Algorithms like Dijkstra's, Bellman-Ford, and A (A-star)* are commonly used to solve shortest path problems in graphs.
